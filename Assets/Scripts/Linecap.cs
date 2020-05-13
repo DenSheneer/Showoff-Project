02653d844cc8d038796435687a3de60d6adaab57
DenGer <452886@student.saxion.nl>
2020-05-13 12:03:15 +0200

    Added linecaps Added start of line prefab

---
+++ Assets/Scripts/Linecap.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Linecap : MonoBehaviour
+{
+    public Vector3 WorldPosition
+    {
+        get { return transform.TransformPoint(Vector3.zero); }
+    }
+    private void Start()
+    {
+        Debug.Log(WorldPosition);
+    }
+}
