f13dac5c99d0e75b00748faa48b6750f2bedbc83
DominiquedeGraafff <dominiqueholtkamp@hotmail.com>
2020-05-20 16:39:19 +0200

    UI update, reset and startscreen

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+   
+    public void PlayGame ()
+    {
+        SceneManager.LoadScene("UIReset");
+
+    }
+
+    public void LanuageGerman ()
+    {
+        SceneManager.LoadScene("SampleScene"); //  german version of game
+    }
+
+    public void LanguageDutch ()
+    {
+        SceneManager.LoadScene("SampleScene"); // dutch  version of game
+    }
+
+    public void Language_English ()
+    {
+        SceneManager.LoadScene("SampleScene"); //english version of game
+    }
+}
