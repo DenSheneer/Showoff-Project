8893ed49ef2f20a20ea5aa5d5ec59c127d84a924
Wessel <467551@student.saxion.nl>
2020-06-04 14:37:25 +0200

    Added highscore list with a scene

---
+++ Assets/Scripts/ScoreListElement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreListElement : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI nameText = null;
+
+    [SerializeField]
+    private TextMeshProUGUI scoreText = null;
+
+    [SerializeField]
+    private TextMeshProUGUI datePlayedText = null;
+
+    public TextMeshProUGUI NameText { get => nameText; }
+    public TextMeshProUGUI ScoreText { get => scoreText; }
+    public TextMeshProUGUI DatePlayedText { get => datePlayedText; }
+
+
+
+}
