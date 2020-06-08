233aff06ca7b3ea72d4d4ebfbc361a320379e89a
Wessel <467551@student.saxion.nl>
2020-06-08 14:38:06 +0200

    started adding editor scripts to help making levels easier

---
+++ Assets/Scripts/Editor/PickupManagerEditor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Security.Cryptography;
+
+[CustomEditor(typeof(PickupManager))]
+public class PickupManagerEditor : Editor
+{
+    private GameObject[] allGameObjects = null;
+    private PickupManager pickupManager = null;
+
+    public void OnEnable()
+    {
+        if (target is PickupManager)
+            pickupManager = target as PickupManager;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        if (GUILayout.Button("Get all tapbles"))
+        {
+            allGameObjects = GameObject.FindObjectsOfType<GameObject>();
+
+            foreach (GameObject gameObject in allGameObjects)
+            {
+                TapAble tapAble = gameObject.GetComponent<TapAble>();
+
+                if (tapAble != null)
+                    pickupManager.AddTapble(tapAble);
+            }
+
+        }
+
+        base.OnInspectorGUI();
+    }
+}
