6a65e7f6b08785e06d8951e38f6d318863c6c7c5
Daan <info.mvdvl@gmail.com>
2020-06-11 17:48:32 +0200

    Force-fixed the render queue on the glass of the lantern Re-baked the navmesh set the rocks and treetrunk to the obstacle layer

---
+++ Assets/RenderQueueFix.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderQueueFix : MonoBehaviour
+{
+    public Renderer renderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        renderer.materials[1].renderQueue = 2900;
+    }
+}
